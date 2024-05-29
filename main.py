103a9881c857a5c3b49d631297694858ea6a91d9
ISSSSA <52818700+ISSSSA@users.noreply.github.com>
2024-05-29 03:25:21 +0300

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class TestApp:
+    def __init__(self):
+        self.name = "Fedor"
+
+    def print_name(self) -> None:
+        print(self.name)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
