98285cde039922387e59a80b53595163d8431391
ISSSSA <52818700+ISSSSA@users.noreply.github.com>
2024-05-29 03:28:20 +0300

    fix bug

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ class TestApp:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = TestApp()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
